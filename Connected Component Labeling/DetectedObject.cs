58c66217135acc46130ccddc9b6c6b5e40f32441
Calvin <calvin97pol@live.nl>
2016-11-12 14:56:34 +0100

    Added bounding box

--- Connected Component Labeling/DetectedObject.cs
+++ Connected Component Labeling/DetectedObject.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
+using System;
+using System.Linq;
 
 public class DetectedObject
 {
     public int id;
     public List<Point> pixels = new List<Point>();
     public List<Point> perimeterPixels = new List<Point>();
+    public Rectangle boundingBox;
 
     // Properties
     public int Area
@@ -50,5 +53,36 @@ public class DetectedObject
     {
         foreach (Point point in perimeterPixels)
             ImageProcessing.imageProcessing.image[point.X, point.Y] = color;
+    }
+
+    public void ColorBoundingBox(Color color)
+    {
+        boundingBox = BoundingBox();
+
+        for (int i = boundingBox.Left; i <= boundingBox.Right; i++)
+        {
+            ImageProcessing.imageProcessing.image[i, boundingBox.Top] = color;
+            ImageProcessing.imageProcessing.image[i, boundingBox.Bottom] = color;
+        }
+
+        for (int j = boundingBox.Top; j <= boundingBox.Bottom; j++)
+        {
+            ImageProcessing.imageProcessing.image[boundingBox.Left, j] = color;
+            ImageProcessing.imageProcessing.image[boundingBox.Right, j] = color;
+        }
+    }
+
+    public Rectangle BoundingBox()
+    {
+        // If the boundingBox of this object has not already been determined, calculate it.
+        int minX = pixels.Min(p => p.X);
+        int maxX = pixels.Max(p => p.X);
+        int minY = pixels.Min(p => p.Y);
+        int maxY = pixels.Max(p => p.Y);
+
+        boundingBox = new Rectangle(minX, minY, maxX - minX, maxY - minY);
+
+        // Return the bounding box.
+        return boundingBox;
     }
 }
